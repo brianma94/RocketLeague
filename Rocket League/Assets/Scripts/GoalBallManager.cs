5e9a4ab7871811630b1d56dd5c8c9c61d4ae0163
brianma94 <brian_1994_bcn@hotmail.com>
2017-05-03 21:02:09 +0200

    goals

---
+++ Rocket League/Assets/Scripts/GoalBallManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalBallManager : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Goal")
+        {
+            GameObject car = GameObject.Find("car_12");
+            GameObject ball = GameObject.Find("Ball");
+            ball.transform.position = GameObject.Find("BallPosition").transform.position;
+            ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            car.transform.position = GameObject.Find("CarPosition").transform.position;
+            car.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+
+}
