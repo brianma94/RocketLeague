b3c4df37382ff59e2a448c5ea3751f0970df9c3e
brianma94 <brian_1994_bcn@hotmail.com>
2017-05-03 10:01:26 +0200

    First sprint

---
+++ Rocket League/Assets/Scripts/camera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour {
+
+    public Transform target1;
+    public Transform target2;
+
+
+    private Vector3 offset = new Vector3(0, 0, 5);
+
+    void Start()
+    {
+        offset = transform.position;
+        transform.LookAt(target1);
+        transform.LookAt(target2);
+        //transform.position += offset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // transform.LookAt(target1);
+        //transform.LookAt(target2);
+       // transform.position += offset;
+    }
+}
