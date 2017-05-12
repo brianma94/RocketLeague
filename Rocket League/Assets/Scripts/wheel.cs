25799ae94d6b60c8551da329591c8cdd88ea7071
eric garcia ribera <eric garcia ribera>
2017-05-12 23:34:36 +0100

    New car

---
+++ Rocket League/Assets/Scripts/wheel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wheel : MonoBehaviour {
+
+	public float rotationMag = 3.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow)) {
+			transform.Rotate(Vector3.right);
+		}
+		if(Input.GetKey(KeyCode.DownArrow)) {
+			transform.Rotate(Vector3.left);
+		}
+	}
+}
