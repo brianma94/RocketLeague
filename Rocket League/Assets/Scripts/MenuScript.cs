09145c0faefc0c62cd4e12c590604f0ef3193973
eric garcia ribera <eric garcia ribera>
2017-05-13 23:40:32 +0100

    commit

---
+++ Rocket League/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour {
+
+	public Canvas startMenu;
+	public Canvas quitMenu;
+	public Button startButton;
+	public Button exitButton;
+
+	// Use this for initialization
+	void Start () {
+		startMenu = startMenu.GetComponent<Canvas> ();
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		startButton = startButton .GetComponent<Button> ();
+		exitButton = exitButton.GetComponent<Button> ();
+
+		quitMenu.enabled = false;
+	}
+
+	public void ExitPress() {
+		quitMenu.enabled = true;
+		startButton.enabled = false;
+		exitButton.enabled = false;
+	}
+
+	public void NoPress() {
+		quitMenu.enabled = false;
+		startButton.enabled = true;
+		exitButton.enabled = true;
+	}
+
+	public void StartLevel() {
+		startMenu.enabled = false;
+	}
+
+	public void ExitGame() {
+		Application.Quit ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
