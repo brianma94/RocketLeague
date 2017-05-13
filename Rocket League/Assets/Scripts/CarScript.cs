3803fa4336eb5ce955065ab446c540c420da685c
eric garcia ribera <eric garcia ribera>
2017-05-13 09:01:31 +0100

    Some things

--- Rocket League/Assets/Scripts/CarScript.cs
+++ Rocket League/Assets/Scripts/CarScript.cs
@@ -6,7 +6,7 @@ public class CarScript : MonoBehaviour {
 
 	public float fMag = 10.0f;
 	public float impulseMag = 100.0f;
-	public float rotationMag = 30.0f;
+	public float rotationMag = 100.0f;
 	// Use this for initialization
 	void Start () {
 		GetComponent<Rigidbody>().centerOfMass = new Vector3(0.0f, -0.3f, 0.0f);
