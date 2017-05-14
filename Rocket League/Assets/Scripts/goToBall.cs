3807c9ff65da9e5478b6f8083a3552b27ecbef68
Brian Martinez <brian_1994_bcn@hotmail.com>
2017-05-14 17:59:41 +0200

    enemyFollowBall

---
+++ Rocket League/Assets/Scripts/goToBall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goToBall : MonoBehaviour {
+	public GameObject ball;
+	void Start(){
+		ball = GameObject.Find("Ball"); 
+	}
+	void Update()
+	{
+		Transform target = ball.transform;
+		float moveSpeed = 5; 
+		float speed = (float)0.3;
+		float rotationSpeed = 5;
+		this.transform.LookAt(target);
+		this.transform.position = Vector3.Lerp( new Vector3(transform.position.x,0,transform.position.z), new Vector3(target.position.x, 0, target.position.z), Time.deltaTime );	
+	}
+}
